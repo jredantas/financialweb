1baf01926a6816b74e3acab1c0c33312fc3b31e2
jredantas <jrdantas@yahoo.com.br>
2017-09-12 01:51:18 -0300

    Layout index e login

--- index.py
+++ index.py
@@ -1,23 +1,59 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 11 16:43:08 2017
+Spyder Editor
 
-@author: 09959295800
+Este é um arquivo de script temporário.
 """
 
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask import request, session, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "It works!"
 
-@app.route('/contact/')
+#@app.route('/')
+#def index():
+#    return "It works !"
+
+@app.route('/')
+def accueil():
+    return render_template('accueil.html', titre='Financial Web')
+
+@app.route('/contact')
 def contact():
     mail = "jrdantas@yahoo.com.br"
     tel = "55 85 99633"
     return "Mail: {} --- Tel: {}".format(mail, tel)
 
+@app.route('/another')
+def another():
+    if 'username' in session:
+        return render_template('accueil.html', titre='Financial Web')
+        #return 'Logged in as %s' % escape(session['username'])
+    #return alert
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html', titre='Financial Web - Login')
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+
+
 if __name__ == '__main__':
+
     app.run(debug=True)
